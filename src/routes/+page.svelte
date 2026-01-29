0a3660a680eabfce0bc6e259267d6645eadccbda
ssebastianoo <sebastianogirotto05@gmail.com>
2026-01-29 11:58:17 +0100

    add contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,36 @@
 			<p class="text-2xl">nomorecho</p>
 		</div>
 		<div>
-			<p class="mb-1 text-lg">listen_us</p>
+			<p class="mb-1 text-lg">canvas_of_you__out_now</p>
 			<div class="grid grid-cols-2 text-pink-400">
 				<a
 					class="hover:underline"
 					target="_blank"
-					href="https://open.spotify.com/artist/4dctRUcKxGPNXcTEk9mzTF">~spotify</a
+					href="https://open.spotify.com/track/0bDw1lezu1DOYZwyThiaMo">~spotify</a
 				>
 				<a
 					class="hover:underline"
 					target="_blank"
-					href="https://music.apple.com/it/artist/nomorecho/1848701396">~apple_music</a
+					href="https://music.apple.com/it/album/canvas-of-you/1849544684">~apple</a
 				>
-				<a class="hover:underline" target="_blank" href="https://tidal.com/artist/68940367"
-					>~tidal</a
+				<a
+					class="hover:underline"
+					target="_blank"
+					href="https://tidal.com/album/469446010/track/469446011">~tidal</a
 				>
-				<a class="hover:underline" target="_blank" href="https://www.deezer.com/en/artist/352878482"
+				<a class="hover:underline" target="_blank" href="https://www.deezer.com/en/album/844769172"
 					>~deezer</a
 				>
+				<a class="hover:underline" target="_blank" href="https://music.amazon.it/tracks/B0FXVR7J3S"
+					>~amazon</a
+				>
+				<a
+					class="hover:underline"
+					target="_blank"
+					href="https://music.youtube.com/watch?v=MZtHvMd6ffM"
+				>
+					~youtube
+				</a>
 			</div>
 		</div>
 
@@ -41,6 +53,14 @@
 				<a class="hover:underline" target="_blank" href="https://www.tiktok.com/@nomorecho"
 					>@tiktok</a
 				>
+			</div>
+		</div>
+
+		<div>
+			<p class="mb-1 text-lg">contact_us</p>
+
+			<div class=" text-pink-400">
+				<a class="hover:underline" href="mailto:nomorechoband@gmail.com">nomorechoband@gmail.com</a>
 			</div>
 		</div>
 
