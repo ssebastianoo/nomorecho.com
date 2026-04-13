feb613acca9fffa465e091914827e07d92d75fe4
ssebastianoo <sebastianogirotto05@gmail.com>
2026-04-13 23:29:27 +0200

    pink arc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import '@fontsource-variable/spline-sans-mono';
+    import '@fontsource/instrument-serif';
 	import './app.css';
 
 	let { children } = $props();
 </script>
 
-<main class="min-h-svh flex flex-col p-8">
-	{@render children()}
-</main>
+{@render children()}
